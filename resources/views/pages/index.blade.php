56449a6b648e0db915ca12918969b6152d7c038e
Kevin Susena <susena.kevin@gmail.com>
2020-11-25 08:33:55 +0700

    commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>{!!$title!!}</h1>
+<ul class="list-group">
+    <li class="list-group-item list-group-item-action list-group-item-dark">Biodata</li>
+    <li class="list-group-item list-group-item-action">AW AW</li>
+    <li class="list-group-item list-group-item-action">Japanese - Madura</li>
+    <li class="list-group-item list-group-item-action">Tanggal Lahir</li>
+    <li class="list-group-item list-group-item-action">Hobby</li>
+  </ul>
+@endsection
